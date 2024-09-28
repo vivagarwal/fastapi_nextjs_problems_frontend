233ed716467461c7ed6fedb879cb31192536c9c3
vivagarwal <vivek.jsr.topper@gmail.com>
2024-09-28 12:17:39 +0530

    added run functionality to send code

--- pages/problems/submit/[id].jsx
+++ pages/problems/submit/[id].jsx
@@ -1,60 +1,48 @@
-import React, { useEffect, useState } from "react";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs";
+import React, { useEffect, useState, useRef } from "react";
+import dynamic from 'next/dynamic';
+import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-c";
 import "prismjs/components/prism-cpp";
 import "prismjs/components/prism-java";
 import "prismjs/components/prism-python";
 import "prismjs/themes/prism-tomorrow.css";
-// import { useRouter } from 'next/router';
+
+// Dynamically import the Editor component
+const Editor = dynamic(() => import("react-simple-code-editor"), { ssr: false });
 
 const ProblemDetails = ({ id }) => {
   const [problemDescription, setProblemDescription] = useState("");
   const [code, setCode] = useState("");
   const [language, setLanguage] = useState("cpp");
   const [output, setOutput] = useState("");
-  const [problem, setProblem] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [input, setInput] = useState("");
   const editorRef = useRef(null);
-  const [submissionResult, setSubmissionResult] = useState({});
-  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    // Disable spell-checking for the entire component
     document.body.spellcheck = false;
-
-    // Re-enable spell-checking when component unmounts
     return () => {
       document.body.spellcheck = true;
     };
   }, []);
 
   useEffect(() => {
-    if (!id) return; // Ensure the id is available
+    if (!id) return;
 
     const fetchProblemDescription = async () => {
       try {
-        console.log(id);
-        console.log(`${process.env.NEXT_PUBLIC_API_URL}`);
-        console.log(
-          `${process.env.NEXT_PUBLIC_API_URL}/api/get-problem-description/${id}`
-        );
         const response = await fetch(
           `${process.env.NEXT_PUBLIC_API_URL}/api/get-problem-description/${id}`
         );
-        console.log(response);
-        const htmlContent = await response.text(); // Fetch HTML as text
-        setProblemDescription(htmlContent); // Set the fetched HTML content
+        const htmlContent = await response.text();
+        setProblemDescription(htmlContent);
       } catch (error) {
         console.error("Error fetching problem description:", error);
       }
     };
 
     fetchProblemDescription();
-  }, [id]); // Re-fetch when the ID changes
+  }, [id]);
 
   const handleRun = async () => {
     if (!id) {
@@ -63,44 +51,35 @@ const ProblemDetails = ({ id }) => {
     }
 
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/run`,
-        {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/run`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
           language,
           code,
           input,
-        }
-      );
-      console.log(response.data);
-      setOutput(response.data.output);
+        }),
+      });
+      const data = await response.json();
+      setOutput(data.output);
     } catch (error) {
       console.error("Error running code:", error);
-      setOutput(
-        "Error: " +
-          (error.response?.data?.error || "An unexpected error occurred")
-      );
+      setOutput("Error: An unexpected error occurred");
     }
   };
 
   return (
-    // <div className="p-4">
-    //   <h1 className="text-2xl font-bold mb-4">Problem Description</h1>
-    //   {/* Rendering the fetched HTML */}
-    //   <div
-    //     dangerouslySetInnerHTML={{ __html: problemDescription }}
-    //     className="problem-description"
-    //   />
-    // </div>
-
     <div className="flex h-screen bg-gray-100 w-screen">
-        <h1 className="text-2xl font-bold mb-4">Problem Description</h1>
-      {/* Problem View (Left Side) */}
+      <h1 className="text-2xl font-bold mb-4">Problem Description</h1>
       <div className="w-1/2 p-6 overflow-y-auto">
-        <div dangerouslySetInnerHTML={{ __html: problemDescription }}
-        className="problem-description"/>
+        <div
+          dangerouslySetInnerHTML={{ __html: problemDescription }}
+          className="problem-description"
+        />
       </div>
 
-      {/* Compiler (Right Side) */}
       <div className="w-1/2 p-6 bg-gray-800 text-white">
         <h3 className="text-white font-bold mb-4">Compile Genie</h3>
         <div className="mb-4">
@@ -122,8 +101,7 @@ const ProblemDetails = ({ id }) => {
         >
           <Editor
             value={code}
-            placeholder="Write your code here"
-            onValueChange={(code) => setCode(code)}
+            onValueChange={setCode}
             highlight={(code) =>
               highlight(code, languages[language] || languages.clike)
             }
@@ -138,20 +116,18 @@ const ProblemDetails = ({ id }) => {
           />
         </div>
 
-        {/* Input textarea */}
         <div className="mt-4">
           <h3 className="text-xl font-semibold mb-2">Custom Input:</h3>
           <textarea
             value={input}
-            placeholder="Enter your input.."
             onChange={(e) => setInput(e.target.value)}
             className="w-full h-1/6 p-4 bg-gray-700 text-white font-mono rounded"
-            spellCheck="false" // This line disables browser spell-checking
-            autoCorrect="off" // This line disables autocorrect on mobile devices
-            autoCapitalize="off" // This line disables auto-capitalization
-            data-gramm="false" // Disables Grammarly
-            data-gramm_editor="false" // Also disables Grammarly
-            data-enable-grammarly="false" // Disables Grammarly
+            spellCheck="false"
+            autoCorrect="off"
+            autoCapitalize="off"
+            data-gramm="false"
+            data-gramm_editor="false"
+            data-enable-grammarly="false"
           />
         </div>
 
@@ -167,7 +143,7 @@ const ProblemDetails = ({ id }) => {
           <h3 className="text-xl font-semibold mb-2">Output:</h3>
           <textarea
             value={output}
-            readOnly // read-only since it's just for displaying output
+            readOnly
             className="w-full h-1/6 p-4 bg-gray-700 text-white font-mono rounded"
           />
         </div>
@@ -176,11 +152,8 @@ const ProblemDetails = ({ id }) => {
   );
 };
 
-// This gets called on every request
 export async function getServerSideProps(context) {
   const { id } = context.params;
-
-  // Pass data to the page via props
   return { props: { id } };
 }
 
